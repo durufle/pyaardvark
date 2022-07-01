33219741ab97877485e60fb4c43d17eb4458c15d
Laurent Bonnet <laurent.bonnet@ul.com>
2022-07-01 08:51:29 +0200

    update: documentation configuration - brand

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,6 +32,7 @@ release = version
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autodoc',
     'sphinx.ext.coverage',
     'sphinx.ext.napoleon',
     'sphinx.ext.intersphinx'
@@ -52,7 +53,6 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = 'classic'
 html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
@@ -62,7 +62,7 @@ html_theme = 'alabaster'
 html_theme_options = {}
 
 # UL Logo
-html_logo = './_images/Ul_Red.png'
+html_logo = './_images/UL_Solutions.png'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
